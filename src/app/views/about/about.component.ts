1c4b184b283670936a8036bb941089ddf62c7a19
ElexemSDdR <valendomta@gmail.com>
2026-06-15 12:29:45 -0300

    Refactorizando el código cambiando toda la sección del social media para componetizarlo en links aparte pasandole por prop la red social que obtengo desde about me traducido

--- src/app/views/about/about.component.ts
+++ src/app/views/about/about.component.ts
@@ -1,16 +1,18 @@
+import { SocialMediaButtonComponent } from '@/app/components/utilities/social-media-button/social-media-button.component'
 import { TranslateService } from '@/app/services/translate.service'
+import { AboutMeContent, SocialNetwork } from '@/types'
 import { Component, inject } from '@angular/core'
-import { SocialMediaSectionComponent } from '@components/core/social-media-section/social-media-section.component'
 import { MarkdownComponent } from 'ngx-markdown'
 
 @Component({
   selector: 'app-about',
-  imports: [SocialMediaSectionComponent, MarkdownComponent],
+  imports: [MarkdownComponent, SocialMediaButtonComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css',
 })
 export class AboutComponent {
   private translateService = inject(TranslateService)
 
-  translatedAbout = this.translateService.getTranslatedAboutMe('es')
+  translatedAbout: AboutMeContent = this.translateService.getTranslatedAboutMe('es')
+  socialNetworks = Object.keys(this.translatedAbout.socialMedia) as SocialNetwork[]
 }
